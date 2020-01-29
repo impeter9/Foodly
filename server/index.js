f7ec95a0088fdd00f92419165e8a139bc80bfd5d
Peter Park <impeter9@gmail.com>
2020-01-28 18:11:39 -0800

    progress on post request from login

--- server/index.js
+++ server/index.js
@@ -4,11 +4,29 @@ const bcrypt = require('bcryptjs');
 const Controller = require('./controller.js');
 const PORT = process.env.port || 3000;
 
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+
 const Model = require('./model.js');
 
 var app = express();
+
+require('./passport')(passport);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
 
 app.use(express.static(__dirname + '/../react-client/dist'));
 
@@ -74,6 +92,10 @@ app.post('/api/register', (req, res) => {
       })
   }
 });
+
+// app.post('/api/login', (req, res) => {
+//   passport.authenticate('local', )
+// });
 
 app.listen(PORT, function() {
   console.log(`listening on port ${PORT}!`);
